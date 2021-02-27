98f16d355e95dd7b6a21e22f909b6c0b0853831c
greggman <github@greggman.com>
2021-02-27 18:03:40 +0000

    Deploying to gh-pages from  @ 1a5c03426115f4209eb0f27c695ca95f36e9cf74 🚀

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function g(prop) {
 }
 function renderUI() {
     ImHUI.start();
-    ImHUI.begin("My First Tool", g('my_first_active'), 'MenuBar');
+    ImHUI.begin("Test Window", g('my_first_active'), 'MenuBar');
     ImHUI.text(`Hello, world 🌐 ${123}`);
     if (ImHUI.button("Save")) {
         console.log('save');
